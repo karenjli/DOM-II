93df288ecc6bfadab52b8e2aa714f9708986333d
karenjli <karenjy.li@gmail.com>
2019-08-06 16:50:38 -0500

    added scroll

--- js/index.js
+++ js/index.js
@@ -9,21 +9,16 @@ const logo = document.querySelector (".main-navigation .container h1");
 logo.addEventListener('mouseover', (e) => logo.style.color = "hotpink");
 
 const subheading = document.querySelectorAll (".content-section h2");
-console.log(subheading);
 subheading.forEach (subheading => {
     subheading.addEventListener('click', (e) => subheading.style.color = "yellow");
 }) 
-
-
-// const page = document.querySelectorAll ("body");
-// page.addEventListener('keydown', (e) => page.style.backgroundColor = "blue");
 
 const page = document.querySelector ('body');
 page.addEventListener('keydown', (e) => page.style.backgroundColor = "lightBlue");
 //window.addEventListener('keydown', (e) => window.style.backgroundColor = "red");
 
-// const topBtn = document.createElement ("button");
-// topBtn.textContent = "Top";
-// topBtn.style.border = solid black;
+window.addEventListener('resize', (e) => {console.log ("The window is resized")});
 
-
+const button = document.querySelectorAll ('.btn')
+button.forEach ( button => {
+    addEventListener('scroll', (e)=> {button.style.borderColor = "pink"})});
