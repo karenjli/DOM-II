4e4e91e97930cb7bd9b29d6b9ccdcb197b79bd85
karenjli <karenjy.li@gmail.com>
2019-08-06 16:14:31 -0500

    added text and background color on DOM

--- js/index.js
+++ js/index.js
@@ -1,1 +1,25 @@
 // Your code goes here
+const headline = document.querySelector(".main-navigation .logo-heading");
+headline.addEventListener ("dblclick", (e) => alert("Welcome to Fun Bus"));
+
+const welcome = document.querySelector (".container .intro h2");
+welcome.addEventListener('click', (e) => console.log ("Welcome"));
+
+const subheading = document.querySelectorAll (".content-section h2");
+console.log(subheading);
+subheading.forEach (subheading => {
+    subheading.addEventListener('click', (e) => subheading.style.color = "yellow");
+}) 
+
+// const page = document.querySelectorAll ("body");
+// page.addEventListener('keydown', (e) => page.style.backgroundColor = "blue");
+
+const page = document.querySelector ('body');
+page.addEventListener('keydown', (e) => page.style.backgroundColor = "lightBlue");
+//window.addEventListener('keydown', (e) => window.style.backgroundColor = "red");
+
+// const topBtn = document.createElement ("button");
+// topBtn.textContent = "Top";
+// topBtn.style.border = solid black;
+
+
